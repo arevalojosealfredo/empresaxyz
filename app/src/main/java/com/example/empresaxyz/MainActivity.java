b03abe2e5aa4ed01549bb8432fc37b75dcd58026
Jose Arevalo <job.38011@gmail.com>
2020-06-17 22:23:11 -0500

    Definición de los strings, de los adapter, falta aun la funcion para el calculo de pago

--- app/src/main/java/com/example/empresaxyz/MainActivity.java
+++ app/src/main/java/com/example/empresaxyz/MainActivity.java
@@ -8,12 +8,14 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText cantidad;
     private TextView valorPagar;
     private Spinner comboMaterial, comboDije, comboTipo, comboMoneda;
-    private ArrayAdapter<String> adapterMaterial, adapterDije, AdapterTipo, AdapterMoneda;
+    private ArrayAdapter<String> adapterMaterial, adapterDije, adapterTipo, adapterMoneda;
     private String[] material, dije, tipo, moneda;
 
     @Override
@@ -22,6 +24,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cantidad = findViewById(R.id.txtCantidad);
+        valorPagar = findViewById(R.id.lblValorPagar);
+        comboMaterial = findViewById(R.id.cmbMaterial);
+        comboDije = findViewById(R.id.cmbDije);
+        comboTipo = findViewById(R.id.cmbTipo);
+        comboMoneda = findViewById(R.id.cmbMoneda);
 
+        material = getResources().getStringArray(R.array.material);
+        dije = getResources().getStringArray(R.array.dije);
+        tipo = getResources().getStringArray(R.array.tipo);
+        moneda = getResources().getStringArray(R.array.moneda);
+
+        adapterMaterial = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, material);
+        comboMaterial.setAdapter(adapterMaterial);
+
+        adapterDije = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, dije);
+        comboDije.setAdapter(adapterDije);
+
+        adapterTipo = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, tipo);
+        comboTipo.setAdapter(adapterTipo);
+
+        adapterMoneda = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, moneda);
+        comboMoneda.setAdapter(adapterMoneda);
     }
+
+    
+
 }
