b63d2cff452e293c41a8b1841f3b8d8907fd6dd5
Jose Arevalo <job.38011@gmail.com>
2020-06-17 21:53:37 -0500

    Main Activity establecido pero aun sin funcionar

--- app/src/main/java/com/example/empresaxyz/MainActivity.java
+++ app/src/main/java/com/example/empresaxyz/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.empresaxyz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText cantidad;
+    private TextView valorPagar;
+    private Spinner comboMaterial, comboDije, comboTipo, comboMoneda;
+    private ArrayAdapter<String> adapterMaterial, adapterDije, AdapterTipo, AdapterMoneda;
+    private String[] material, dije, tipo, moneda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cantidad = findViewById(R.id.txtCantidad);
+
     }
 }
